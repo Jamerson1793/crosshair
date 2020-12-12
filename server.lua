16b91d792b0d50bbdd76d26db6ce3279cabc18aa
Jamerson1793 <cwmcb123@gmail.com>
2020-12-11 21:08:18 -0500

    fixed my dumbass mistake

--- server.lua
+++ server.lua
@@ -6,7 +6,7 @@ local on - true
 RegisterCommand('global_ctoggle', function()
 --- PUT PERMS CHECK FOR YOUR FRAMEWORK HERE
 	if on then
-		on - false
+		on = false
 	elseif not on then
 		on = true
 	end
