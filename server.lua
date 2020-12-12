d6a2489ecec1b364bb75a2ce8b1c2201f6a6f041
Jamerson1793 <cwmcb123@gmail.com>
2020-12-11 20:54:25 -0500

    commented some shit

--- server.lua
+++ server.lua
@@ -1,5 +1,8 @@
 local on - true
 
+
+
+--- This is to toggle the crosshair for the whole server, you need to setup perms for your framework
 RegisterCommand('global_ctoggle', function()
 --- PUT PERMS CHECK FOR YOUR FRAMEWORK HERE
 	if on then
